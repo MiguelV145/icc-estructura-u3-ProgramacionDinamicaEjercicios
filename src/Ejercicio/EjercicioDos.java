c861035f14c5582e84b3febbaf7398b953f7b217
miguelv145 <miguelvanegas145@gmail.com>
2024-07-15 18:05:37 -0500

    trabajo mejorado

--- src/Ejercicio/EjercicioDos.java
+++ src/Ejercicio/EjercicioDos.java
@@ -1,4 +1,7 @@
 package Ejercicio;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /*
  * Dados n pares de paréntesis, implementa un algoritmo para generar todas las combinaciones
@@ -20,8 +23,38 @@ package Ejercicio;
  * Se debe usar StringBuilder
  */
 public class EjercicioDos {
+   
+   public List<String> generateParenthesis(int n) {
+    List<String> result = new ArrayList<>();
+    generate(result, new StringBuilder(), 0, 0, n);
+  
+    return result;
+}
 
-    public List<List<Integer>> subsets(List<Integer> set) {
+private void generate(List<String> result, StringBuilder current, int open, int close, int max) {
+    
+    if (current.length() == max * 2) {
+      
+        result.add(current.toString());
+        return;
+    }
 
-     }
+   
+    if (open < max) {
+        
+        current.append('(');
+        
+        generate(result, current, open + 1, close, max);
+       
+        current.deleteCharAt(current.length() - 1);
+    }
+
+    if (close < open) {
+
+        current.append(')');
+        generate(result, current, open, close + 1, max);
+        current.deleteCharAt(current.length() - 1);
+    }
+}
+
 }
